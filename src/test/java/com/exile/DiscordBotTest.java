a4f7175ea0de9d281200b1b7ff39e9de77159a78
ricksterhd123 <ricksterhd123@gmail.com>
2022-11-19 08:42:07 +0000

    Update test

--- src/test/java/com/exile/DiscordBotTest.java
+++ src/test/java/com/exile/DiscordBotTest.java
@@ -1,14 +1,29 @@
 package com.exile;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
 public class DiscordBotTest 
 {
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        List mockedList = mock(List.class);
+
+        mockedList.add("one");
+        mockedList.clear();
+
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
+
+        when(mockedList.get(0)).thenReturn("hello");
+        when(mockedList.get(1)).thenThrow(new RuntimeException());
+
+        assertEquals("hello", mockedList.get(0));
+        assertThrows(RuntimeException.class, () -> mockedList.get(1));
     }
 }
